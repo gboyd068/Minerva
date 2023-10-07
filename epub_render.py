e7686bf0b9ec67faa528e53e67cde51abf9a2218
Gregory Boyd <mert4517@ox.ac.uk>
2023-10-07 12:15:27 +0100

    add next and prev buttons

--- epub_render.py
+++ epub_render.py
@@ -3,10 +3,10 @@ from ebooklib import epub
 from bs4 import BeautifulSoup
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
 
 
 class EPUBReaderApp(App):
@@ -14,9 +14,17 @@ class EPUBReaderApp(App):
         super(EPUBReaderApp, self).__init__()
         self.epub_file = epub_file
         self.book = None
+        self.current_item_index = 0
+        self.book_items_list = []
+        self.num_book_items = 0
+        self.chapter_label = None
+        self.scroll_view = None
 
     def build(self):
         self.load_epub()
+        self.book_items_list = list(
+            self.book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+        self.num_book_items = len(self.book_items_list)
         return self.create_ui()
 
     def load_epub(self):
@@ -27,40 +35,60 @@ class EPUBReaderApp(App):
         main_layout = BoxLayout(orientation="vertical")
 
         # Create a ScrollView to hold the content
-        scroll_view = ScrollView()
+        self.scroll_view = ScrollView()
 
-        # Create a GridLayout to display chapters
-        grid_layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        grid_layout.bind(minimum_height=grid_layout.setter('height'))
+        # Create a Label to display the chapter content
+        self.chapter_label = Label(
+            markup=True,
+            size_hint_y=None,
+            halign='left',
+            valign='top',
+            padding=(0, 0),  # Adjust padding as needed
+            text_size=(Window.width - 200, None),  # Set text_size to wrap text
+        )
+        self.chapter_label.bind(texture_size=self.chapter_label.setter('size'))
 
-        # Iterate through the book's items (chapters) and display them
-        for itemidx, item in enumerate(self.book.get_items()):
-            if item.get_type() == ebooklib.ITEM_DOCUMENT and itemidx == 11:
+        # Create a Button layout for navigation
+        button_layout = BoxLayout(size_hint=(1, 0.1))
+        prev_button = Button(text="Previous", on_press=self.prev_chapter)
+        next_button = Button(text="Next", on_press=self.next_chapter)
+        button_layout.add_widget(prev_button)
+        button_layout.add_widget(next_button)
+
+        # Add the Label and Button layouts to the main layout
+        main_layout.add_widget(self.scroll_view)
+        self.scroll_view.add_widget(self.chapter_label)
+        main_layout.add_widget(button_layout)
+
+        # Display the first chapter
+        self.display_chapter(self.chapter_label)
+
+        return main_layout
+
+    def display_chapter(self, label):
+        if 0 <= self.current_item_index < self.num_book_items:
+            item = self.book_items_list[self.current_item_index]
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
                 # Use BeautifulSoup to parse and extract HTML content
                 soup = BeautifulSoup(item.content, "html.parser")
                 chapter_text = soup.get_text()
+                label.text = chapter_text
 
-                # Create a Label to display the chapter content
-                chapter_label = Label(
-                    text=chapter_text,
-                    markup=True,
-                    size_hint_y=None,
-                    halign='left',
-                    valign='top',
-                    padding=(0, 0),  # Adjust padding as needed
-                    # Set text_size to wrap text
-                    text_size=(Window.width - 200, None),
-                )
-                chapter_label.bind(texture_size=chapter_label.setter('size'))
-                grid_layout.add_widget(chapter_label)
+    def prev_chapter(self, instance):
+        if self.current_item_index > 0:
+            self.current_item_index -= 1
+            self.display_chapter(self.chapter_label)
 
-        # Add the GridLayout to the ScrollView
-        scroll_view.add_widget(grid_layout)
+            # Scroll to the bottom of the ScrollView
+            self.scroll_view.scroll_y = 0.0
 
-        # Add the ScrollView to the main layout
-        main_layout.add_widget(scroll_view)
+    def next_chapter(self, instance):
+        if self.current_item_index < self.num_book_items - 1:
+            self.current_item_index += 1
+            self.display_chapter(self.chapter_label)
 
-        return main_layout
+            # Scroll to the top of the ScrollView
+            self.scroll_view.scroll_y = 1.0
 
 
 if __name__ == "__main__":
