b452794e3aeb07abaa37d1237c72c23a79837518
Gregory Boyd <mert4517@ox.ac.uk>
2023-10-09 19:12:32 +0100

    add top toolbar hiding

--- src/player_screen.py
+++ src/player_screen.py
@@ -1,11 +1,21 @@
 from kivy.uix.screenmanager import Screen
 from kivy.animation import Animation
+from kivy.core.window import Window
 
 
 class PlayerScreen(Screen):
-    def show_toolbar(self):
-        if self.ids.toolbar.y < 0:
-            Animation(y=0, duration=0.2).start(self.ids.toolbar)
+    def show_audio_toolbar(self):
+        if self.ids.audio_toolbar.y < 0:
+            Animation(y=0, duration=0.2).start(self.ids.audio_toolbar)
         else:
-            Animation(y=-self.ids.toolbar.height,
-                      duration=0.2).start(self.ids.toolbar)
+            Animation(y=-self.ids.audio_toolbar.height,
+                      duration=0.2).start(self.ids.audio_toolbar)
+
+    def show_top_toolbar(self):
+        window_down_y = Window.height - self.ids.top_toolbar.height
+        if self.ids.top_toolbar.y > window_down_y:
+            Animation(y=window_down_y, duration=0.2).start(
+                self.ids.top_toolbar)
+        else:
+            Animation(y=Window.height,
+                      duration=0.2).start(self.ids.top_toolbar)
