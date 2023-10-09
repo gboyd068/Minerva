9bf36d028d04f515b3dccf565fc653ded6cf137f
Gregory Boyd <mert4517@ox.ac.uk>
2023-10-09 19:04:34 +0100

    add player layout, add settings page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.settings import SettingsWithSidebar
+
 from src.library_screen import LibraryScreen
 from src.player_screen import PlayerScreen
 
@@ -14,7 +16,18 @@ kv = Builder.load_file("main.kv")
 
 class MyMainApp(App):
     def build(self):
+        self.settings_cls = SettingsWithSidebar
+        self.use_kivy_settings = False
         return kv
+
+    def build_config(self, config):
+        pass
+
+    def build_settings(self, settings):
+        pass
+
+    def on_config_change(self, config, section, key, value):
+        pass
 
 
 if __name__ == "__main__":
--- src/player_screen.py
+++ src/player_screen.py
@@ -1,5 +1,11 @@
 from kivy.uix.screenmanager import Screen
+from kivy.animation import Animation
 
 
 class PlayerScreen(Screen):
-    pass
+    def show_toolbar(self):
+        if self.ids.toolbar.y < 0:
+            Animation(y=0, duration=0.2).start(self.ids.toolbar)
+        else:
+            Animation(y=-self.ids.toolbar.height,
+                      duration=0.2).start(self.ids.toolbar)
