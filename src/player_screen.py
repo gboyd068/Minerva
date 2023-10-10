e2ba1e507cdeb25f39fe076235a435118799c5e1
Gregory Boyd <mert4517@ox.ac.uk>
2023-10-10 17:36:06 +0100

    make audio toolbar adjust size of reader window

--- src/player_screen.py
+++ src/player_screen.py
@@ -7,11 +7,19 @@ from kivy.uix.label import Label
 
 class PlayerScreen(Screen):
     def toggle_audio_toolbar(self):
+        duration = 0.2
         if self.ids.audio_toolbar.y < 0:
-            Animation(y=0, duration=0.2).start(self.ids.audio_toolbar)
+            # move the audio toolbar up
+            Animation(y=0, duration=duration).start(self.ids.audio_toolbar)
+            # move the reader window up to make room for the audio toolbar
+            Animation(size=(Window.width, Window.height - self.ids.audio_toolbar.height),
+                      duration=duration).start(self.ids.reader_window)
         else:
             Animation(y=-self.ids.audio_toolbar.height,
-                      duration=0.2).start(self.ids.audio_toolbar)
+                      duration=duration).start(self.ids.audio_toolbar)
+            # move the reader window down
+            Animation(size=(Window.width, Window.height),
+                      duration=duration).start(self.ids.reader_window)
 
     def toggle_top_toolbar(self):
         window_down_y = Window.height - self.ids.top_toolbar.height
