d6932bb97ae779d750ac6cfb37df7c6dab3c1207
Gregory Boyd <mert4517@ox.ac.uk>
2023-10-10 20:16:54 +0100

    toolbar logic and icons

--- src/player_screen.py
+++ src/player_screen.py
@@ -3,36 +3,53 @@ from kivy.animation import Animation
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.label import Label
+from kivymd.uix.button import MDIconButton
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import BooleanProperty, NumericProperty
 
 
 class PlayerScreen(Screen):
-    def toggle_audio_toolbar(self):
-        duration = 0.2
-        if self.ids.audio_toolbar.y < 0:
-            # move the audio toolbar up
-            Animation(y=0, duration=duration).start(self.ids.audio_toolbar)
-            # move the reader window up to make room for the audio toolbar
-            Animation(size=(Window.width, Window.height - self.ids.audio_toolbar.height),
-                      duration=duration).start(self.ids.reader_window)
-        else:
-            Animation(y=-self.ids.audio_toolbar.height,
-                      duration=duration).start(self.ids.audio_toolbar)
-            # move the reader window down
-            Animation(size=(Window.width, Window.height),
-                      duration=duration).start(self.ids.reader_window)
+    pass
 
-    def toggle_top_toolbar(self):
-        window_down_y = Window.height - self.ids.top_toolbar.height
-        if self.ids.top_toolbar.y > window_down_y:
-            Animation(y=window_down_y, duration=0.2).start(
-                self.ids.top_toolbar)
+
+class MyToolbar(BoxLayout):
+    # give the toolbar properties that can be used to animate it
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.is_active = BooleanProperty(False)
+        self.resize_reader_window = BooleanProperty(False)
+        self.inactive_y = NumericProperty(0)
+        self.active_y = NumericProperty(0)
+
+    def toggle_toolbar(self):
+        if self.is_active:
+            Animation(y=self.inactive_y,
+                      duration=0.2).start(self)
         else:
-            Animation(y=Window.height,
-                      duration=0.2).start(self.ids.top_toolbar)
+            Animation(y=self.active_y, duration=0.2).start(
+                self)
+        self.is_active = not self.is_active
+
+        # if the toolbar is meant to resize the reader window, do so
+        if self.resize_reader_window:
+            if self.is_active:
+                Animation(size=(Window.width, Window.height - self.height),
+                          duration=0.2).start(self.parent.parent.ids.reader_window)
+            else:
+                Animation(size=(Window.width, Window.height),
+                          duration=0.2).start(self.parent.parent.ids.reader_window)
+
+
+class BottomToolbarButton(MDIconButton):
+    pass
 
 
 class ReaderWindow(Label):
-    pass
+    def on_touch_down(self, touch):
+        # logic for clicking on the reader window
+        top_toolbar = self.parent.ids.top_toolbar
+        if top_toolbar.is_active:
+            top_toolbar.toggle_toolbar()
 
 
 class TransparentButton(Button):
